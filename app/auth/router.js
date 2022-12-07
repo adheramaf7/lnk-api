065cae46d88b308dd48d364fc9c899d97ceb6fad
Adhe Rama F <adheramaf7@gmail.com>
2022-12-07 13:44:12 +0700

    init

---
+++ app/auth/router.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const multer = require('multer');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const controller = require('./controller');
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, controller.localStrategy));
+
+router.post('/login', multer().none(), controller.login);
+router.post('/logout', controller.logout);
+
+module.exports = router;
